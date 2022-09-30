7780d0fb6b9cac979c0c070a4318f89f6dbe8d0c
Anshu Kumar <114074097+anshu6635@users.noreply.github.com>
2022-09-29 23:29:08 -0700

    Add files via upload

---
+++ Leb Session/29-09-2022/Parent.java
@@ -0,0 +1,16 @@
+package CoreJava;
+/*Ques1:create a class parent with default constructor and void sing method.print something inside the constructor and method
+//Create child class which will inherit the property of parent and has methods void speak and void dance.print something inside the two methods.
+Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program
+@author :- Anshu Kumar
+Date    :- Thursday,sep 29
+ */
+ class Parent
+ {
+	 //Creating method's 
+	 public void sing()
+	 {
+		 System.out.println("Anshu father is a singer");
+	 }
+
+}
