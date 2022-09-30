e6b0ebdb81a979e986c0a359d72c6ef78c6c6fe7
Anshu Kumar <114074097+anshu6635@users.noreply.github.com>
2022-09-30 00:10:44 -0700

    Add files via upload

---
+++ Practical/Student_Encapsulation.java
@@ -0,0 +1,23 @@
+package CoreJava;
+
+class Student_Encapsulation {
+//Encapsulation is something that -->binding data with method's
+	private int rollno;
+	private String name;
+	
+	// Getter and Setter
+	
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
