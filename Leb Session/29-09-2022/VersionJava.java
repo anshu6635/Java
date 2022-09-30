51fb8cc899f82d5fe693057cdb6b4d6c81c70d2c
Anshu Kumar <114074097+anshu6635@users.noreply.github.com>
2022-09-29 23:33:05 -0700

    Add files via upload

---
+++ Leb Session/29-09-2022/VersionJava.java
@@ -0,0 +1,18 @@
+package CoreJava;
+/*
+ program to find version of java
+ @author :- Anshu Kumar
+Date    :- Thursday,sep 29
+ */
+
+ class VersionJava
+ {
+	 //Callling main method
+	 public static void main(String args[]) 
+	 {
+		 //printing version of java
+		 System.out.println("Version of the java is :"+System.getProperty("java.version"));
+	 }
+	 
+
+}
