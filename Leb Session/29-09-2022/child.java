5845e1cc3e56f709643d4815af33d1c9b8ccc9bd
Anshu Kumar <114074097+anshu6635@users.noreply.github.com>
2022-09-29 23:33:30 -0700

    Add files via upload

---
+++ Leb Session/29-09-2022/child.java
@@ -0,0 +1,18 @@
+package CoreJava;
+//inheriting parent class in this child class
+
+class child extends Parent
+{
+//Creating method's of this class
+	// Method :1
+	public void speak() 
+	{
+		System.out.println("Anshu is a good speaker");
+	}
+	//Method:2
+	public void dance()
+	{
+		System.out.println("Anshu is not a good dancer");
+	}
+
+}
