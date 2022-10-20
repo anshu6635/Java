502cd172cd6bd86c14c46491a527d07371149729
Anshu Kumar <114074097+anshu6635@users.noreply.github.com>
2022-10-20 14:04:02 +0530

    Add files via upload

---
+++ Practical/Array.java
@@ -0,0 +1,23 @@
+package CoreJava;
+
+public class Array {
+
+	public static void main(String[] args) 
+	{
+		//Declaring and instantiating the Aarry
+		int a[] = new int[3];
+		//intialiaztion
+		a[0] = 12;
+		a[1] = 4;
+		a[2] = 6;
+		
+		//Treaversing the array
+		for(int i = 0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+				
+
+	}
+
+}
