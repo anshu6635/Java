3de607e91d44b8d9b87448dbfbaccf6690db4101
Anshu Kumar <114074097+anshu6635@users.noreply.github.com>
2022-10-20 14:04:35 +0530

    Add files via upload

---
+++ Practical/ArrayString.java
@@ -0,0 +1,22 @@
+package CoreJava;
+//Write a program to Declare and Traverse   string Array
+
+public class ArrayString {
+
+	public static void main(String[] args)
+	{ 
+		//Declaration and instantiation of string array
+		
+		String a[] = {"Anshu","Rahul","Priyana"};
+		
+		//Traversing 
+		for (int i = 0 ; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+		
+
+	}
+
+}
